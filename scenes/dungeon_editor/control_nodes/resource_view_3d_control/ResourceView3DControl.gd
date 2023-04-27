1ee9934d7d3cdca8a9fb9ae65e2e511551ab6e48
Bryan Miller <bmiller1008@gmail.com>
2023-04-27 16:47:05 -0400

    Technically finished core work on ResourceView3DControl, but ultimately untested. Started to create the Dungeon Editor ActiveCellControls scene, but only JUST started.

--- scenes/dungeon_editor/control_nodes/resource_view_3d_control/ResourceView3DControl.gd
+++ scenes/dungeon_editor/control_nodes/resource_view_3d_control/ResourceView3DControl.gd
@@ -1,3 +1,4 @@
+@tool
 extends Control
 
 
@@ -19,18 +20,29 @@ const THEME_STYLE_PRESS : StringName = &"press"
 # Export Variables
 # ------------------------------------------------------------------------------
 @export_category("Resource View 3D Control")
-@export var lookup_table_name : StringName = ""
-@export var meta_resource_name : StringName = ""
-@export var camera_height : float = 1.0
-@export_range(-90.0, 90.0, 0.1) var camera_pitch_degrees : float = 0.0
-@export var camera_zoom : float = 2.0
+@export_group("Resource")
+@export var lookup_table_name : StringName = &"":							set = set_lookup_table_name
+@export var resource_section : StringName = &"":							set = set_resource_section
+@export var resource_name : StringName = &"":								set = set_resource_name
+@export_group("Camera")
+@export var camera_height : float = 1.0:									set = set_camera_height
+@export_range(-90.0, 90.0, 0.1) var camera_pitch_degrees : float = 0.0:		set = set_camera_pitch_degrees
+@export var camera_zoom : float = 2.0:										set = set_camera_zoom
+@export_group("Animation")
+@export var enable_seesaw : bool = true:									set = set_enable_seesaw
+@export_range(0.0, 180.0, 0.1) var arc_degrees : float = 90.0:				set = set_arc_degrees
+@export var duration : float = 1.0:											set = set_duration
 
 # ------------------------------------------------------------------------------
 # Variables
 # ------------------------------------------------------------------------------
-var _mouse_button_down : bool = false
+var _press_active : bool = false
 var _mouse_entered : bool = false
 var _focus_active : bool = false
+
+var _anim_active : bool = false
+
+var _resource_node : Node3D = null
 
 # ------------------------------------------------------------------------------
 # Onready Variables
@@ -42,10 +54,77 @@ var _focus_active : bool = false
 @onready var _pitch : Node3D = %Pitch
 
 # ------------------------------------------------------------------------------
+# Setters
+# ------------------------------------------------------------------------------
+func set_lookup_table_name(ltn : StringName) -> void:
+	if ltn != lookup_table_name:
+		lookup_table_name = ltn
+		_UpdateResourceNode()
+
+func set_resource_section(rs : StringName) -> void:
+	if rs != resource_section:
+		resource_section = rs
+		_UpdateResourceNode()
+
+func set_resource_name(rn : StringName) -> void:
+	if rn != resource_name:
+		resource_name = rn
+		_UpdateResourceNode()
+
+func set_camera_height(h : float) -> void:
+	if h != camera_height:
+		camera_height = h
+		_UpdateCameraPlacement()
+
+func set_camera_pitch_degrees(p : float) -> void:
+	if p >= -90.0 and p <= 90.0 and p != camera_pitch_degrees:
+		camera_pitch_degrees = p
+		_UpdateCameraPlacement()
+
+func set_camera_zoom(z : float) -> void:
+	if z >= 0.0 and z != camera_zoom:
+		camera_zoom = z
+		_UpdateCameraPlacement()
+
+func set_enable_seesaw(e : bool) -> void:
+	if e != enable_seesaw:
+		enable_seesaw = e
+		if enable_seesaw:
+			_AnimCamera()
+
+func set_arc_degrees(d : float) -> void:
+	if d >= 0.0 and d <= 180.0 and d != arc_degrees:
+		arc_degrees = d
+
+func set_duration(d : float) -> void:
+	if d >= 0.0 and d != duration:
+		duration = d
+
+# ------------------------------------------------------------------------------
 # Override Methods
 # ------------------------------------------------------------------------------
 func _ready() -> void:
 	_UpdatePanelStyle()
+	_UpdateCameraPlacement()
+	_UpdateResourceNode()
+	if enable_seesaw:
+		_AnimCamera()
+
+func _gui_input(event : InputEvent) -> void:
+	if _mouse_entered and is_instance_of(event, InputEventMouseButton):
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			_press_active = event.is_pressed()
+			_UpdatePanelStyle()
+			accept_event()
+	elif _focus_active:
+		if event.is_action_pressed("ui_accept"):
+			_press_active = true
+			_UpdatePanelStyle()
+			accept_event()
+		elif event.is_action_released("ui_accept"):
+			_press_active = false
+			_UpdatePanelStyle()
+			accept_event()
 
 func _notification(what : int) -> void:
 	match what:
@@ -79,24 +158,24 @@ func _GetThemeType() -> StringName:
 func _GetThemeStyleBox(style_name : StringName) -> StyleBox:
 	return get_theme_stylebox(style_name, _GetThemeType())
 
-func _GetThemeFont(font_name : StringName) -> Font:
-	return get_theme_font(font_name, _GetThemeType())
-
-func _GetThemeFontSize(font_size_name : StringName) -> int:
-	return get_theme_font_size(font_size_name, _GetThemeType())
-
-func _GetThemeColor(color_name : StringName) -> Color:
-	var tt : StringName = _GetThemeType()
-	if has_theme_color(color_name, tt):
-		return get_theme_color(color_name, tt)
-	return Color.BLACK
-
-func _GetThemeConstant(const_name : StringName) -> int:
-	return get_theme_constant(const_name, _GetThemeType())
+#func _GetThemeFont(font_name : StringName) -> Font:
+#	return get_theme_font(font_name, _GetThemeType())
+#
+#func _GetThemeFontSize(font_size_name : StringName) -> int:
+#	return get_theme_font_size(font_size_name, _GetThemeType())
+#
+#func _GetThemeColor(color_name : StringName) -> Color:
+#	var tt : StringName = _GetThemeType()
+#	if has_theme_color(color_name, tt):
+#		return get_theme_color(color_name, tt)
+#	return Color.BLACK
+#
+#func _GetThemeConstant(const_name : StringName) -> int:
+#	return get_theme_constant(const_name, _GetThemeType())
 
 func _UpdatePanelStyle() -> void:
 	if _cpanel == null: return
-	if _mouse_button_down:
+	if _press_active:
 		_cpanel.add_theme_stylebox_override(&"panel", _GetThemeStyleBox(THEME_STYLE_PRESS))
 	elif _mouse_entered:
 		_cpanel.add_theme_stylebox_override(&"panel", _GetThemeStyleBox(THEME_STYLE_HOVER))
@@ -107,6 +186,67 @@ func _UpdatePanelStyle() -> void:
 
 func _UpdateCameraPlacement() -> void:
 	if _gimble == null or _pitch == null or _camera == null: return
+	_gimble.position.y = camera_height
+	_pitch.rotation_degrees.x = camera_pitch_degrees
+	_camera.position.z = camera_zoom
+
+func _UpdateResourceNode() -> void:
+	if Engine.is_editor_hint(): return # Don't update this in editor!
+	if _sub_viewport == null: return
+	if _resource_node != null:
+		_sub_viewport.remove_child(_resource_node)
+		_resource_node.queue_free()
+		_resource_node = null
+	if lookup_table_name == &"" or resource_section == &"" or resource_name == &"": return
+	
+	var mrlt : CrawlMRLT = Crawl.get_lookup_table(lookup_table_name)
+	if mrlt == null:
+		printerr("ResourceView3DControl [", self.name, "]: No lookup table named \"", lookup_table_name, "\"")
+		return
+	
+	_resource_node = mrlt.load_meta_resource(resource_section, resource_name, true)
+	if _resource_node == null:
+		printerr("ResourceView3DControl [", self.name, "]: Failed to get resource node \"", resource_section, ":", resource_name, "\"")
+
+
+func _AnimCamera() -> void:
+	if Engine.is_editor_hint(): return # Don't do this in editor!
+	if _anim_active or not enable_seesaw: return
+	if arc_degrees <= 0.0 or duration <= 0.0: return
+	
+	# Just in case arc_degrees or duration gets changed in the middle of the animation.
+	var arc : float = arc_degrees
+	var dur : float = duration
+	
+	_anim_active = true
+	var tween : Tween = create_tween()
+	tween.set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(_gimble, "rotation_degrees:z", arc * 0.5, dur * 0.5)
+	await(tween.finished)
+	
+	if not enable_seesaw:
+		_gimble.rotation_degrees.z = 0.0
+		_anim_active = false
+		return
+	
+	tween = create_tween()
+	tween.set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(_gimble, "rotation_degrees:z", -(arc * 0.5), dur)
+	await(tween.finished)
+	
+	if not enable_seesaw:
+		_gimble.rotation_degrees.z = 0.0
+		_anim_active = false
+		return
+	
+	tween = create_tween()
+	tween.set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(_gimble, "rotation_degrees:z", 0.0, dur * 0.5)
+	tween.finished.connect(
+		func():
+			_anim_active = false
+			_AnimCamera.call_deferred()
+	)
 	
 
 # ------------------------------------------------------------------------------
