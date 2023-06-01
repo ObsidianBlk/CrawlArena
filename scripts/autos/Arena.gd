0dc6a50042f4737cb89436360c645b4df3621bbc
Bryan Miller <bmiller1008@gmail.com>
2023-06-01 13:59:45 -0400

    Reorganized Arena autoload script. Started working on a 'keyring' system for storing Twitch client and secret keys... as well as similar information from other services.

--- scripts/autos/Arena.gd
+++ scripts/autos/Arena.gd
@@ -1,6 +1,31 @@
 extends Node
 
+# ------------------------------------------------------------------------------
+# Constants
+# ------------------------------------------------------------------------------
+const LOOKUPTABLE_LEVEL_GEOMETRY : StringName = &"level_geometry"
+const LOOKUPTABLE_ENTITIES : StringName = &"entities"
+
+const KEYRING_TAG : String = "ARENA_KEYRING:1"
+const KEYRING_SERVICE_PREFIX : String = "@:"
+# ------------------------------------------------------------------------------
+# Variables
+# ------------------------------------------------------------------------------
+var _key_ring : Dictionary = {}
+
+# ------------------------------------------------------------------------------
+# Override Methods
+# ------------------------------------------------------------------------------
 func _ready() -> void:
+	_DefineLevelResources()
+	_DefineEntityResources()
+
+# ------------------------------------------------------------------------------
+# Private Methods
+# ------------------------------------------------------------------------------
+func _DefineLevelResources() -> void:
+	if Crawl.has_lookup_table(LOOKUPTABLE_LEVEL_GEOMETRY): return
+	
 	var lglt : CrawlMRLT = CrawlMRLT.new()
 	lglt.register_from_dictionary({
 		"ground":{
@@ -34,7 +59,11 @@ func _ready() -> void:
 			}
 		}
 	})
-	Crawl.store_lookup_table(&"level_geometry", lglt)
+	Crawl.store_lookup_table(LOOKUPTABLE_LEVEL_GEOMETRY, lglt)
+
+
+func _DefineEntityResources() -> void:
+	if Crawl.has_lookup_table(LOOKUPTABLE_ENTITIES): return
 	
 	var elt : CrawlMRLT = CrawlMRLT.new()
 	elt.register_from_dictionary({
@@ -50,4 +79,70 @@ func _ready() -> void:
 			}
 		}
 	})
-	Crawl.store_lookup_table(&"entities", elt)
+	Crawl.store_lookup_table(LOOKUPTABLE_ENTITIES, elt)
+
+func _LoadRingEntry(file : FileAccess) -> Dictionary:
+	var ring : Dictionary = {}
+	
+	var service : String = file.get_line()
+	if service.length() <= KEYRING_SERVICE_PREFIX.length() or not service.begins_with(KEYRING_SERVICE_PREFIX):
+		printerr("Keyring load error. Expected service name.")
+		return {}
+	service = service.right(service.length() - KEYRING_SERVICE_PREFIX.length()).strip_edges()
+	if service.is_empty():
+		printerr("Keyring load error. Service name is empty.")
+		return {}
+	ring[service] = {}
+	
+	var propstr : String = file.get_line()
+	var properties : PackedStringArray = propstr.split("&", false)
+	if properties.size() <= 0:
+		printerr("Keyring load error. Failed to find properties for service \"", service, "\".")
+		return {}
+	for prop in properties:
+		prop = prop.strip_edges()
+		if prop.is_empty():
+			printerr("Keyring load error. Property field empty for service \"", service, "\".")
+			return {}
+		ring[service][prop] = ""
+	
+	for _i in range(ring[service].size()):
+		pass # TODO: Finish this!!
+	return ring
+
+# ------------------------------------------------------------------------------
+# Public Methods
+# ------------------------------------------------------------------------------
+func load_keyring(filepath : String, encrypt_passphrase : String = "") -> int:
+	if not filepath.is_absolute_path():
+		printerr("Absolute path to keyring file required.")
+		return ERR_FILE_BAD_PATH
+	
+	if not FileAccess.file_exists(filepath):
+		printerr("No keyring file found.")
+		return ERR_FILE_NOT_FOUND
+	
+	var file : FileAccess = FileAccess.open_encrypted_with_pass(
+		filepath, FileAccess.READ, encrypt_passphrase
+	)
+	if file == null:
+		printerr("Failed to open keyring file.")
+		return FileAccess.get_open_error()
+	
+	var tag : String = file.get_line()
+	if tag != KEYRING_TAG:
+		printerr("Keyring missing identifier tag.")
+		file.close()
+		return ERR_INVALID_DECLARATION
+	
+	var service : String = ""
+	var ring : Dictionary = {}
+	while not file.eof_reached():
+		if service == "":
+			service = file.get_line()
+			ring.clear()
+	
+	return OK
+
+
+
